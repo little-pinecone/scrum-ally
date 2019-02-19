9944766f93e5506ae2891f1b47a31d054e094e0d
little_pinecone <marta.szymek.mail@gmail.com>
2019-02-19 15:11:15 +0100

    feature/11 Unused code removed.

--- backend/src/main/java/in/keepgrowing/scrumally/config/MvcConfiguration.java
+++ backend/src/main/java/in/keepgrowing/scrumally/config/MvcConfiguration.java
@@ -28,8 +28,5 @@ public class MvcConfiguration implements WebMvcConfigurer {
                 });
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
