41185bfcce8578ce51a992bfabbe3b445b23f5a4
little-pinecone <marta.szymek.mail@gmail.com>
2020-08-04 14:37:22 +0200

    Fix test configuration.

--- backend/src/test/java/in/keepgrowing/scrumally/ScrumAllyApplicationTests.java
+++ backend/src/test/java/in/keepgrowing/scrumally/ScrumAllyApplicationTests.java
@@ -3,14 +3,16 @@ package in.keepgrowing.scrumally;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@TestPropertySource(properties = {"spring.flyway.enabled=false"})
 public class ScrumAllyApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
