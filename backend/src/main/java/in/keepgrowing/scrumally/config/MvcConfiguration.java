951ba8dc0fd805c21daf8a483ab730b19f30677f
little-pinecone <marta.szymek.mail@gmail.com>
2020-11-28 16:32:56 +0100

    Update Springfox to v3.0.0

--- backend/src/main/java/in/keepgrowing/scrumally/config/MvcConfiguration.java
+++ backend/src/main/java/in/keepgrowing/scrumally/config/MvcConfiguration.java
@@ -3,7 +3,9 @@ package in.keepgrowing.scrumally.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
@@ -12,8 +14,11 @@ import java.io.IOException;
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
 
+    private final String baseUrl = "api";
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        String baseUrl = StringUtils.trimTrailingCharacter(this.baseUrl, '/');
         registry.addResourceHandler("/**")
                 .addResourceLocations("classpath:/static/")
                 .resourceChain(true)
@@ -26,7 +31,15 @@ public class MvcConfiguration implements WebMvcConfigurer {
                                 : new ClassPathResource("/static/index.html");
                     }
                 });
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.
+                addResourceHandler(baseUrl + "/swagger-ui/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+                .resourceChain(false);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController(baseUrl + "/swagger-ui/")
+                .setViewName("forward:" + baseUrl + "/swagger-ui/index.html");
     }
 }
--- backend/src/main/java/in/keepgrowing/scrumally/config/SwaggerConfig.java
+++ backend/src/main/java/in/keepgrowing/scrumally/config/SwaggerConfig.java
@@ -6,12 +6,10 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
-@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
